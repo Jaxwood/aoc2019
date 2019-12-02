5599d2bd0e5936585db33dedebc7089ec9b45005
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2019-12-02 08:45:15 +0100

    Testcases pass

--- src/aoc2019/day02/core.clj
+++ src/aoc2019/day02/core.clj
@@ -1,6 +1,27 @@
 (ns aoc2019.day02.core)
 
+;; update a position to a value
+;; (update [1 2 3] 2 (constantly 10))
+;; [1 2 10]
+
+;; get from a position
+;; (get [1 2 3] 1)
+;; 2
+
+(defn getOperation
+  "get the operation based on val"
+  [val op1 op2]
+  (if (= val 1)
+    (+ op1 op2)
+    (* op1 op2)))
+
 (defn intCode
   "calculates int codes for a sequence"
-  [sequence]
-  sequence)
+  [sequence idx]
+  (let [val (get sequence idx)]
+    (if (= val 99)
+      sequence
+      (let [opcode1 (get sequence (get sequence (+ idx 1)))
+            opcode2 (get sequence (get sequence (+ idx 2)))
+            newIdx (get sequence (+ idx 3))]
+        (intCode (update sequence newIdx (constantly (getOperation val opcode1 opcode2))) (+ idx 4))))))
--- test/aoc2019/day02_test.clj
+++ test/aoc2019/day02_test.clj
@@ -5,6 +5,26 @@
 (deftest day02
   (testing "a"
     (def expected [3500 9 10 70 2 3 11 0 99 30 40 50])
-    (def actual (intCode [1 9 10 3 2 3 11 0 99 30 40 50]))
+    (def actual (intCode [1 9 10 3 2 3 11 0 99 30 40 50] 0))
+    (is (= expected actual)))
+  (testing "b"
+    (def expected [2 0 0 0 99])
+    (def actual (intCode [1 0 0 0 99] 0))
+    (is (= expected actual)))
+  (testing "c"
+    (def expected [2 3 0 6 99])
+    (def actual (intCode [2 3 0 3 99] 0))
+    (is (= expected actual)))
+  (testing "d"
+    (def expected [2 4 4 5 99 9801])
+    (def actual (intCode [2 4 4 5 99 0] 0))
+    (is (= expected actual)))
+  (testing "e"
+    (def expected [30 1 1 4 2 5 6 0 99])
+    (def actual (intCode [1 1 1 4 99 5 6 0 99] 0))
+    (is (= expected actual)))
+  (testing "getOperation"
+    (def expected 6)
+    (def actual (getOperation 2 2 3))
     (is (= expected actual)))
 )
