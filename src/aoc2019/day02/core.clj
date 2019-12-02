b429167199d0b37d0aec1bee098da429f981fb06
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2019-12-02 07:06:57 +0100

    Add failing testcase

--- src/aoc2019/day02/core.clj
+++ src/aoc2019/day02/core.clj
@@ -1,2 +1,6 @@
 (ns aoc2019.day02.core)
 
+(defn intCode
+  "calculates int codes for a sequence"
+  [sequence]
+  sequence)
--- test/aoc2019/day02_test.clj
+++ test/aoc2019/day02_test.clj
@@ -4,7 +4,7 @@
 
 (deftest day02
   (testing "a"
-    (def expected 2)
-    (def actual 3)
+    (def expected [3500 9 10 70 2 3 11 0 99 30 40 50])
+    (def actual (intCode [1 9 10 3 2 3 11 0 99 30 40 50]))
     (is (= expected actual)))
 )
