187dced449e45035616474f266579c4eb40077ca
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2019-12-03 20:54:08 +0100

    Add more testcases

--- test/aoc2019/day03_test.clj
+++ test/aoc2019/day03_test.clj
@@ -7,6 +7,14 @@
     (def expected 6)
     (def actual (day03a ["R8,U5,L5,D3", "U7,R6,D4,L4"]))
     (is (= expected actual)))
+  (testing "b"
+    (def expected 159)
+    (def actual (day03a ["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"]))
+    (is (= expected actual)))
+  (testing "c"
+    (def expected 135)
+    (def actual (day03a ["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"]))
+    (is (= expected actual)))
   (testing "parse"
     (def expected ["R8" "U5" "L5" "D3"]))
     (def actual (parse "R8,U5,L5,D3"))
