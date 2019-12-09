806d29c3b68309f50c006abb4a8be55bbd81674d
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2019-12-09 23:03:25 +0100

    Format code

--- src/aoc2019/day08/core.clj
+++ src/aoc2019/day08/core.clj
@@ -6,14 +6,12 @@
   (let [zeros (count (filter #(= 0 %) image))
         ones (count (filter #(= 1 %) image))
         twos (count (filter #(= 2 %) image))]
-  {
-    :zero zeros
-    :result (* ones twos)
-  }))
+    {:zero zeros
+     :result (* ones twos)}))
 
 (defn day08a
   "Find the best quality layer"
   [filename]
   (let [raw (slurp filename)
         parsed (flatten (vector (map #(Integer/parseInt (str %)) raw)))]
-  (:result (apply min-key :zero (map metadata (partition (* 25 6) parsed))))))
+    (:result (apply min-key :zero (map metadata (partition (* 25 6) parsed))))))
