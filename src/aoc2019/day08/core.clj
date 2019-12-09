2fd5e06c13294cacb023c258deb23036b2831c85
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2019-12-09 23:04:02 +0100

    Fix spelling

--- src/aoc2019/day08/core.clj
+++ src/aoc2019/day08/core.clj
@@ -1,17 +1,19 @@
 (ns aoc2019.day08.core)
 
 (defn metadata
-  "Extract imag metadata"
+  "Extract image metadata"
   [image]
   (let [zeros (count (filter #(= 0 %) image))
         ones (count (filter #(= 1 %) image))
         twos (count (filter #(= 2 %) image))]
-    {:zero zeros
-     :result (* ones twos)}))
+  {
+    :zero zeros
+    :result (* ones twos)
+  }))
 
 (defn day08a
   "Find the best quality layer"
   [filename]
   (let [raw (slurp filename)
         parsed (flatten (vector (map #(Integer/parseInt (str %)) raw)))]
-    (:result (apply min-key :zero (map metadata (partition (* 25 6) parsed))))))
+  (:result (apply min-key :zero (map metadata (partition (* 25 6) parsed))))))
