5d82355dd33680dd06d391eeb135239694e9579a
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2019-12-12 09:11:45 +0100

    Refactor day07

--- src/aoc2019/day07/core.clj
+++ src/aoc2019/day07/core.clj
@@ -38,59 +38,62 @@
     6 3
     7 4
     8 4
-    (throw (Exception. "Unsupported opcode"))))
+    9 op))
+
+(defn print-instruction
+  "prints the instruction"
+  [op]
+  (case op
+    1 (println "add")
+    2 (println "multiply")
+    3 (println "input")
+    4 (println "output")
+    5 (println "jump-if-true")
+    6 (println "jump-if-false")
+    7 (println "less than")
+    8 (println "equals")
+    (println "unknown" op)))
 
 (defn opcode
-  "op: 1: adds together positions of parameters 1, 2 and stores at parameter 3
-       2: multiplies together positions of parameters 1, 2 and stores at parameter 3
-       3: takes input and saves to position of parameter 1
-       4: output value at parameter 1
-   pm: 0: position mode
-       1: immediate mode
-   a:     parameter 3
-   b:     parameter 2
-   c:     parameter 1"
-  [arr idx default acc]
+  ""
+  [arr idx signals]
   (let [[a b c _ op] (digits (get arr idx) [])
         newIdx (+ idx (pointer op))
         fst (get arr (+ idx 1))
         snd (get arr (+ idx 2))
         thd (get arr (+ idx 3))]
+    ;;(print-instruction op)
     (case op
-      1 (opcode (update arr thd (constantly (+ (lookup arr b snd) (lookup arr c fst)))) newIdx default acc)
-      2 (opcode (update arr thd (constantly (* (lookup arr b snd) (lookup arr c fst)))) newIdx default acc)
-      3 (if (= 2 (count default))
-          (opcode (update arr fst (constantly (first default))) newIdx (drop 1 default) acc)
-          (opcode (update arr fst (constantly (first default))) newIdx default acc))
+      1 (opcode (update arr thd (constantly (+ (lookup arr b snd) (lookup arr c fst)))) newIdx signals)
+      2 (opcode (update arr thd (constantly (* (lookup arr b snd) (lookup arr c fst)))) newIdx signals)
+      3 (opcode (update arr fst (constantly (first signals))) newIdx (drop 1 signals))
       4 (let [val (lookup arr c fst)]
           (if (= 0 val)
-            (opcode arr newIdx default (cons val acc))
-            (cons val acc)))
+            (opcode arr newIdx signals)
+            val))
       5 (if (= 0 (lookup arr c fst))
-          (opcode arr newIdx default acc)
-          (opcode arr (lookup arr b snd) default acc))
+          (opcode arr newIdx signals)
+          (opcode arr (lookup arr b snd) signals))
       6 (if (= 0 (lookup arr c fst))
-          (opcode arr (lookup arr b snd) default acc)
-          (opcode arr newIdx default acc))
+          (opcode arr (lookup arr b snd) signals)
+          (opcode arr newIdx signals))
       7 (if (< (lookup arr c fst) (lookup arr b snd))
-          (opcode (update arr thd (constantly 1)) newIdx default acc)
-          (opcode (update arr thd (constantly 0)) newIdx default acc))
+          (opcode (update arr thd (constantly 1)) newIdx signals)
+          (opcode (update arr thd (constantly 0)) newIdx signals))
       8 (if (= (lookup arr c fst) (lookup arr b snd))
-          (opcode (update arr thd (constantly 1)) newIdx default acc)
-          (opcode (update arr thd (constantly 0)) newIdx default acc))
-      (throw (Exception. "Unsupported opcode")))))
+          (opcode (update arr thd (constantly 1)) newIdx signals)
+          (opcode (update arr thd (constantly 0)) newIdx signals))
+      9 0)))
 
 (defn thrusters
   "find highest signal that can be sent to the thrusters"
   [program settings idx]
-  (try
-    (let [a (first (opcode program idx [(get settings 0) 0] []))
-          b (first (opcode program idx [(get settings 1) a] []))
-          c (first (opcode program idx [(get settings 2) b] []))
-          d (first (opcode program idx [(get settings 3) c] []))
-          e (first (opcode program idx [(get settings 4) d] []))]
-      e)
-    (catch Exception e 0)))
+  (let [a (opcode program idx [(get settings 0) 0])
+        b (opcode program idx [(get settings 1) a])
+        c (opcode program idx [(get settings 2) b])
+        d (opcode program idx [(get settings 3) c])
+        e (opcode program idx [(get settings 4) d])]
+    e))
 
 (defn day07a
   "find the highest signal"
