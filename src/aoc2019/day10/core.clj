7eb15473ee4702838377133216e7e676cdf801ea
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2019-12-13 10:34:43 +0100

    Add line of sight function

--- src/aoc2019/day10/core.clj
+++ src/aoc2019/day10/core.clj
@@ -1,1 +1,16 @@
 (ns aoc2019.day10.core)
+
+(defn los?
+  "Given two coordinates and a coordinate system
+   examine if there is line of sight between the coordinates
+   https://www.geeksforgeeks.org/program-check-three-points-collinear/"
+  [galaxy [x1 y1 :as x] [x2 y2 :as y]]
+  (if (empty? galaxy)
+    true
+    (let [[x3 y3 :as z] (first galaxy)
+          a (* x1 (- y2 y3))
+          b (* x2 (- y3 y1))
+          c (* x3 (- y1 y2))]
+      (if (and (= 0 (+ a b c)) (not (= x z)) (not (= y z)))
+        false
+        (recur (rest galaxy) x y)))))
--- test/aoc2019/day10_test.clj
+++ test/aoc2019/day10_test.clj
@@ -3,7 +3,11 @@
             [aoc2019.day10.core :refer :all]))
 
 (deftest day10
-  (testing "FIX ME"
-    (def expected 0) 
-    (def actual 0)
+  (testing "line of sight"
+    (def expected false) 
+    (def actual (los? [[1 0] [4 0] [0 2] [1 2] [2 2] [3 2] [4 2] [4 3] [3 4] [4 4]] [3 4] [1 0]))
+    (is (= expected actual)))
+  (testing "line of sight"
+    (def expected true) 
+    (def actual (los? [[1 0] [4 0] [0 2] [1 2] [2 2] [3 2] [4 2] [4 3] [3 4] [4 4]] [3 4] [1 2]))
     (is (= expected actual))))
