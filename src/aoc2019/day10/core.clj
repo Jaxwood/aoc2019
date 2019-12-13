6fb9f2556cec1ecb8f45915d61f63f074722b2ea
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2019-12-13 18:10:44 +0100

    Add intercepting?

--- src/aoc2019/day10/core.clj
+++ src/aoc2019/day10/core.clj
@@ -19,6 +19,15 @@
         lines (clojure.string/split raw #"\r\n")]
     (lines->galaxy lines 0 [])))
 
+(defn intercepting?
+  ""
+  [[x1 y1 :as x] [x2 y2 :as y] [x3 y3 :as z]]
+    (let [xMax (max x1 x2)
+          xMin (min x1 x2)
+          yMin (min y1 y2)
+          yMax (max y1 y2)]
+    (and (<= x3 xMax) (>= x3 xMin) (<= y3 yMax) (>= y3 yMin))))
+
 (defn los?
   "Given two coordinates and a coordinate system
    examine if there is line of sight between the coordinates
@@ -30,21 +39,6 @@
           a (* x1 (- y2 y3))
           b (* x2 (- y3 y1))
           c (* x3 (- y1 y2))]
-      (if (and (= 0 (+ a b c)) (not (= x z)) (not (= y z)))
+      (if (and (= 0 (+ a b c)) (not (= x z)) (not (= y z)) (intercepting? x y z))
         false
         (recur (rest galaxy) x y)))))
-
-(defn planet-counter
-  "Counts to number of visible neighbor planets in the galaxy"
-  [galaxy candidate acc]
-  (if (empty? galaxy)
-    acc
-    (let [target (first galaxy)]
-      (if (= true (los? galaxy candidate target))
-        (recur (rest galaxy) candidate (inc acc))
-        (recur (rest galaxy) candidate acc)))))
-
-(defn find-radar-station
-  "finds the planet with most visible neighbor planets"
-  [galaxy]
-  (apply max (map #(planet-counter galaxy %1 0) galaxy)))
--- test/aoc2019/day10_test.clj
+++ test/aoc2019/day10_test.clj
@@ -3,23 +3,24 @@
             [aoc2019.day10.core :refer :all]))
 
 (deftest day10
-  (testing "line of sight"
+  (def planets [[1 0] [4 0] [0 2] [1 2] [2 2] [3 2] [4 2] [4 3] [3 4] [4 4]])
+  (testing "los? - return false when planet blocks"
     (def expected false) 
-    (def actual (los? [[1 0] [4 0] [0 2] [1 2] [2 2] [3 2] [4 2] [4 3] [3 4] [4 4]] [3 4] [1 0]))
+    (def actual (los? planets [3 4] [1 0]))
     (is (= expected actual)))
-  (testing "line of sight"
+  (testing "los? - return true if no planet blocks"
     (def expected true) 
-    (def actual (los? [[1 0] [4 0] [0 2] [1 2] [2 2] [3 2] [4 2] [4 3] [3 4] [4 4]] [3 4] [1 2]))
+    (def actual (los? planets [3 4] [2 2]))
     (is (= expected actual)))
-  (testing "parse"
+  (testing "intercepting? - return false if the planets is not intercepting"
+    (def expected false) 
+    (def actual (intercepting? [3 4] [2 2] [1 0]))
+    (is (= expected actual)))
+  (testing "intercepting? - return true the planets is intercepting"
+    (def expected true) 
+    (def actual (intercepting? [3 4] [1 0] [2 2]))
+    (is (= expected actual)))
+  (testing "parse - returns planets as list of tuples"
     (def expected [[1 0] [4 0] [0 2] [1 2] [2 2] [3 2] [4 2] [4 3] [3 4] [4 4]]) 
     (def actual (parse "src/aoc2019/day10/a.txt"))
-    (is (= expected actual)))
-  (testing "count visible planets"
-    (def expected 8)
-    (def actual (planet-counter [[1 0] [4 0] [0 2] [1 2] [2 2] [3 2] [4 2] [4 3] [3 4] [4 4]] [3 4] 0))
-    (is (= expected actual)))
-  (testing "find radar station")
-    (def expected 8)
-    (def actual (find-radar-station [[1 0] [4 0] [0 2] [1 2] [2 2] [3 2] [4 2] [4 3] [3 4] [4 4]]))
-    (is (= expected actual)))
+    (is (= expected actual))))
