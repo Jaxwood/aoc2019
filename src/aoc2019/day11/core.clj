1709756e7db75212f5cfd6ac78d0cb90feec622f
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2019-12-21 08:28:06 +0100

    Add files for day11

---
+++ src/aoc2019/day11/core.clj
@@ -0,0 +1,6 @@
+(ns aoc2019.day11.core)
+
+(defn day11a
+  ""
+  []
+  0)
---
+++ test/aoc2019/day11_test.clj
@@ -0,0 +1,9 @@
+(ns aoc2019.day11-test
+  (:require [clojure.test :refer :all]
+            [aoc2019.day11.core :refer :all]))
+
+(deftest day11
+  (testing "FIX ME"
+    (def expected false) 
+    (def actual true)
+    (is (= expected actual))))
