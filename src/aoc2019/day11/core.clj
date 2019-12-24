3a6719cb2d7219d176ce1a873c151a8ab043d92d
Jaxwood <lorenzen.jacob@gmail.com>
2019-12-24 17:40:57 +0100

    Add move function
    Move function takes a state and a direction to turn (:left or :right) and
    calculate next state

--- src/aoc2019/day11/core.clj
+++ src/aoc2019/day11/core.clj
@@ -1,4 +1,38 @@
-(ns aoc2019.day11.core)
+(ns aoc2019.day11.core
+  (:require [aoc2019.day09.core :refer [opcode]]
+            [clojure.core.match :refer [match]]))
+
+;;  :program []
+;;  :pointer 0
+;;  :signals []
+;;  :base 0
+
+(def initial {:position [0 0]
+              :direction :north
+              :breadcrumbs []})
+
+(defn move
+  "Takes a state and a direction (:left or :right) and calculates next state"
+  [state next]
+  (let [direction (:direction state)
+        [x y :as position] (:position state)
+        breadcrumbs (:breadcrumbs state)]
+    (match [direction next]
+      [:north :left] {:breadcrumbs (conj breadcrumbs position) :direction :west :position [(dec x) y]}
+      [:north :right] {:breadcrumbs (conj breadcrumbs position) :direction :east :position [(inc x) y]}
+      [:south :left] {:breadcrumbs (conj breadcrumbs position) :direction :east :position [(inc x) y]}
+      [:south :right] {:breadcrumbs (conj breadcrumbs position) :direction :west :position [(dec x) y]}
+      [:west :left] {:breadcrumbs (conj breadcrumbs position) :direction :south :position [x (dec y)]}
+      [:west :right] {:breadcrumbs (conj breadcrumbs position) :direction :north :position [x (inc y)]}
+      [:east :left] {:breadcrumbs (conj breadcrumbs position) :direction :north :position [x (inc y)]}
+      [:east :right] {:breadcrumbs (conj breadcrumbs position) :direction :south :position [x (dec y)]})))
+
+(defn blah
+  ""
+  [instructions]
+  (let [a (opcode {:program instructions :pointer 0 :signals [0] :base 0})
+        b (opcode {:program (:program a) :pointer (:pointer a) :signals [(:output a)] :base (:base a)})]
+    [(:output a) (:output b)]))
 
 (defn day11a
   ""
--- test/aoc2019/day11_test.clj
+++ test/aoc2019/day11_test.clj
@@ -1,10 +1,38 @@
 (ns aoc2019.day11-test
   (:require [clojure.test :refer :all]
-            [aoc2019.day09.core :refer :all]
+            [aoc2019.day09.core :refer [parse]]
             [aoc2019.day11.core :refer :all]))
 
 (deftest day11
-  (testing "FIX ME"
-    (def expected []) 
-    (def actual (parse "src/aoc2019/day11/input.txt"))
+  (testing "facing north move left"
+    (def expected {:direction :west :breadcrumbs [[0 0]] :position [-1 0]}) 
+    (def actual (move {:direction :north :breadcrumbs [] :position [0 0] } :left))
+    (is (= expected actual)))
+  (testing "facing north move right"
+    (def expected {:direction :east :breadcrumbs [[0 0]] :position [1 0]}) 
+    (def actual (move {:direction :north :breadcrumbs [] :position [0 0] } :right))
+    (is (= expected actual)))
+  (testing "facing south move left"
+    (def expected {:direction :east :breadcrumbs [[0 0]] :position [1 0]}) 
+    (def actual (move {:direction :south :breadcrumbs [] :position [0 0] } :left))
+    (is (= expected actual)))
+  (testing "facing south move right"
+    (def expected {:direction :west :breadcrumbs [[0 0]] :position [-1 0]}) 
+    (def actual (move {:direction :south :breadcrumbs [] :position [0 0] } :right))
+    (is (= expected actual)))
+  (testing "facing west move left"
+    (def expected {:direction :south :breadcrumbs [[0 0]] :position [0 -1]}) 
+    (def actual (move {:direction :west :breadcrumbs [] :position [0 0] } :left))
+    (is (= expected actual)))
+  (testing "facing west move right"
+    (def expected {:direction :north :breadcrumbs [[0 0]] :position [0 1]}) 
+    (def actual (move {:direction :west :breadcrumbs [] :position [0 0] } :right))
+    (is (= expected actual)))
+  (testing "facing east move left"
+    (def expected {:direction :north :breadcrumbs [[0 0]] :position [0 1]}) 
+    (def actual (move {:direction :east :breadcrumbs [] :position [0 0] } :left))
+    (is (= expected actual)))
+  (testing "facing east move right"
+    (def expected {:direction :south :breadcrumbs [[0 0]] :position [0 -1]}) 
+    (def actual (move {:direction :east :breadcrumbs [] :position [0 0] } :right))
     (is (= expected actual))))
