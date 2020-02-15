f20e5ff0a7a0de9ddbbceb7f21a1689885253cb2
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2020-02-15 08:10:34 +0100

    Add first two instructions to intCode program
    - Add: will add the next two instructions
    - Multiply: will multiply the next two instructions

--- src/intcode/core.clj
+++ src/intcode/core.clj
@@ -4,12 +4,34 @@
   "parse instructions as a raw comma delimited string
    and puts it into a vector"
   [filename]
-    (def raw (slurp filename))
-    (def firstLine (first (clojure.string/split raw #"\r\n")))
-    (def sequences (map #(Long/valueOf %1) (clojure.string/split firstLine #",")))
-    (into [] sequences))
+  (def raw (slurp filename))
+  (def firstLine (first (clojure.string/split raw #"\r\n")))
+  (def sequences (map #(Long/valueOf %1) (clojure.string/split firstLine #",")))
+  (into [] sequences))
+
+(defn add
+  "adds two positions and stores in the third position
+   returns the updated instruction vector"
+  [instructions pointer]
+  (let [position1 (get instructions (+ pointer 1))
+        position2 (get instructions (+ pointer 2))
+        positionToUpdate (get instructions (+ pointer 3))]
+    (update instructions positionToUpdate (constantly (+ (get instructions position1) (get instructions position2))))))
+
+(defn multiply
+  "multiplies two positions and stores in the third position
+   returns the updated instruction vector"
+  [instructions pointer]
+  (let [position1 (get instructions (+ pointer 1))
+        position2 (get instructions (+ pointer 2))
+        positionToUpdate (get instructions (+ pointer 3))]
+    (update instructions positionToUpdate (constantly (* (get instructions position1) (get instructions position2))))))
 
 (defn run
   ""
-  [instructions]
-  0)
+  [instructions pointer]
+  (let [opcode (get instructions pointer)]
+    (case opcode
+      1 (recur (add instructions pointer) (+ pointer 4))
+      2 (recur (multiply instructions pointer) (+ pointer 4))
+      99 instructions)))
--- test/intcode/core_test.clj
+++ test/intcode/core_test.clj
@@ -6,4 +6,16 @@
   (testing "parse"
     (def expected [1 9 10 3 2 3 11 0 99 30 40 50])
     (def actual (parse "test/intcode/parse.txt"))
-    (is (= expected actual))))
+    (is (= expected actual)))
+  (testing "add"
+    (def expected [1 9 10 70 2 3 11 0 99 30 40 50])
+    (def actual (add [1 9 10 3 2 3 11 0 99 30 40 50] 0))
+    (is (= expected actual)))
+  (testing "multiply"
+    (def expected [3500 9 10 70 2 3 11 0 99 30 40 50])
+    (def actual (multiply [1 9 10 70 2 3 11 0 99 30 40 50] 4))
+    (is (= expected actual)))
+  (testing "run"
+    (def expected 3500)
+    (def actual (run [1 9 10 3 2 3 11 0 99 30 40 50] 0))
+    (is (= expected (first actual)))))
