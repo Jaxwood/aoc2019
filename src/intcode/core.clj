c2374f5666ff6db35fc004e70c70a2018b02ef1d
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2020-02-15 08:48:22 +0100

    Refactor run function
    Introduce concepts as instruction, parameters and address

--- src/intcode/core.clj
+++ src/intcode/core.clj
@@ -9,29 +9,34 @@
   (def sequences (map #(Long/valueOf %1) (clojure.string/split firstLine #",")))
   (into [] sequences))
 
+(defn readInstruction
+  "Read the next instruction"
+  [memory address]
+  (let [opcode (get memory address)]
+    (case opcode
+      1 {:opcode opcode :parameters (conj [] (get memory (+ address 1)) (get memory (+ address 2)) (get memory (+ address 3)))}
+      2 {:opcode opcode :parameters (conj [] (get memory (+ address 1)) (get memory (+ address 2)) (get memory (+ address 3)))}
+      99 {:opcode opcode :parameters []})))
+
 (defn add
   "adds two positions and stores in the third position
-   returns the updated instruction vector"
-  [instructions pointer]
-  (let [position1 (get instructions (+ pointer 1))
-        position2 (get instructions (+ pointer 2))
-        positionToUpdate (get instructions (+ pointer 3))]
-    (update instructions positionToUpdate (constantly (+ (get instructions position1) (get instructions position2))))))
+   returns the updated memory"
+  [memory instruction]
+  (let [[a b c] (:parameters instruction)]
+    (update memory c (constantly (+ (get memory a) (get memory b))))))
 
 (defn multiply
   "multiplies two positions and stores in the third position
-   returns the updated instruction vector"
-  [instructions pointer]
-  (let [position1 (get instructions (+ pointer 1))
-        position2 (get instructions (+ pointer 2))
-        positionToUpdate (get instructions (+ pointer 3))]
-    (update instructions positionToUpdate (constantly (* (get instructions position1) (get instructions position2))))))
+   returns the updated memory"
+  [memory instruction]
+  (let [[a b c] (:parameters instruction)]
+    (update memory c (constantly (* (get memory a) (get memory b))))))
 
 (defn run
-  ""
-  [instructions pointer]
-  (let [opcode (get instructions pointer)]
-    (case opcode
-      1 (recur (add instructions pointer) (+ pointer 4))
-      2 (recur (multiply instructions pointer) (+ pointer 4))
-      99 instructions)))
+  "Run the program"
+  [memory address]
+  (let [instruction (readInstruction memory address)]
+    (case (:opcode instruction)
+      1 (recur (add memory instruction) (+ address 4))
+      2 (recur (multiply memory instruction) (+ address 4))
+      99 memory)))
--- test/intcode/core_test.clj
+++ test/intcode/core_test.clj
@@ -8,12 +8,14 @@
     (def actual (parse "test/intcode/parse.txt"))
     (is (= expected actual)))
   (testing "add"
+    (def memory [1 9 10 3 2 3 11 0 99 30 40 50])
     (def expected [1 9 10 70 2 3 11 0 99 30 40 50])
-    (def actual (add [1 9 10 3 2 3 11 0 99 30 40 50] 0))
+    (def actual (add memory (readInstruction memory 0)))
     (is (= expected actual)))
   (testing "multiply"
+    (def memory [1 9 10 70 2 3 11 0 99 30 40 50])
     (def expected [3500 9 10 70 2 3 11 0 99 30 40 50])
-    (def actual (multiply [1 9 10 70 2 3 11 0 99 30 40 50] 4))
+    (def actual (multiply memory (readInstruction memory 4)))
     (is (= expected actual)))
   (testing "run"
     (def expected 3500)
