becf9bc4c12509a9ff058064238164f5470018d4
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2020-02-18 17:24:21 +0100

    Use intcode on day02

--- src/aoc2019/day02/core.clj
+++ src/aoc2019/day02/core.clj
@@ -1,60 +1,29 @@
-(ns aoc2019.day02.core)
-
-(defn getOperation
-  "Get the operation based on val"
-  [val op1 op2]
-  (if (= val 1)
-    (+ op1 op2)
-    (* op1 op2)))
-
-(defn intCode
-  "Calculates int codes for a sequence"
-  [sequence idx]
-  (let [val (get sequence idx)]
-    (if (= val 99)
-      sequence
-      (let [opcode1 (get sequence (get sequence (+ idx 1)))
-            opcode2 (get sequence (get sequence (+ idx 2)))
-            newIdx (get sequence (+ idx 3))]
-        (intCode (update sequence newIdx (constantly (getOperation val opcode1 opcode2))) (+ idx 4))))))
-
-(defn modifyParameters
-  "Updates noun and verb"
-  [seq noun verb]
-  (def updatedSeq (update (update seq 1 (constantly noun)) 2 (constantly verb)))
-  updatedSeq)
-
-(defn incVerb
+(ns aoc2019.day02.core
+  (:require [intcode.core :refer [run]]))
+(defn inc-verb
   "Increment verb"
   [verb]
   (if (= verb 99)
     0
     (inc verb)))
 
-(defn incNoun
+(defn inc-noun
   "Increment noun"
   [noun verb]
   (if (= verb 99)
     (inc noun)
     noun))
 
-(defn parse
-  "Read from input file"
-  [filename]
-  (def raw (slurp filename))
-  (def firstLine (first (clojure.string/split raw #"\r\n")))
-  (def sequences (map #(Integer/parseInt %1) (clojure.string/split firstLine #",")))
-  (into [] sequences))
-
 (defn day02a
   "Calculate int codes for puzzle input"
-  [sequences noun verb]
-  (first (intCode (modifyParameters sequences noun verb) 0)))
+  [program]
+  (first (:memory (run program))))
 
 (defn day02b
   "Calculate int codes for puzzle input"
-  [sequences noun verb target]
-  (let [result (day02a sequences noun verb)]
+  [program noun verb target]
+  (let [memory (assoc (assoc (:memory program) 1 noun) 2 verb)
+        result (day02a (run (assoc program :memory memory)))]
     (if (= result target)
       (+ (* 100 noun) verb)
-      (recur sequences (incNoun noun verb) (incVerb verb) target))))
+      (recur program (inc-noun noun verb) (inc-verb verb) target))))
--- test/aoc2019/day02_test.clj
+++ test/aoc2019/day02_test.clj
@@ -1,37 +1,15 @@
 (ns aoc2019.day02-test
   (:require [clojure.test :refer :all]
+            [intcode.core :refer [parse]]
             [aoc2019.day02.core :refer :all]))
 
 (deftest day02
-  (testing "a"
-    (def expected [3500 9 10 70 2 3 11 0 99 30 40 50])
-    (def actual (intCode [1 9 10 3 2 3 11 0 99 30 40 50] 0))
-    (is (= expected actual)))
-  (testing "b"
-    (def expected [2 0 0 0 99])
-    (def actual (intCode [1 0 0 0 99] 0))
-    (is (= expected actual)))
-  (testing "c"
-    (def expected [2 3 0 6 99])
-    (def actual (intCode [2 3 0 3 99] 0))
-    (is (= expected actual)))
-  (testing "d"
-    (def expected [2 4 4 5 99 9801])
-    (def actual (intCode [2 4 4 5 99 0] 0))
-    (is (= expected actual)))
-  (testing "e"
-    (def expected [30 1 1 4 2 5 6 0 99])
-    (def actual (intCode [1 1 1 4 99 5 6 0 99] 0))
-    (is (= expected actual)))
-  (testing "getOperation"
-    (def expected 6)
-    (def actual (getOperation 2 2 3))
-    (is (= expected actual)))
   (testing "day02a"
     (def expected 3101878)
-    (def actual (day02a (parse "src/aoc2019/day02/input.txt") 12 2))
+    (def memory (assoc (assoc (parse "src/aoc2019/day02/input.txt") 1 12) 2 2))
+    (def actual (day02a {:memory memory :address 0 :relative 0 :input []}))
     (is (= expected actual)))
   (testing "day02b"
     (def expected 8444)
-    (def actual (day02b (parse "src/aoc2019/day02/input.txt") 0 0 19690720))
+    (def actual (day02b {:memory (parse "src/aoc2019/day02/input.txt") :address 0 :relative 0 :input []} 0 0 19690720))
     (is (= expected actual))))
