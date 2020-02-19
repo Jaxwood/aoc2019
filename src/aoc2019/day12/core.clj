a8547cfc3450f1337be37bf9ed10359efb059d45
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2020-02-19 18:14:43 +0100

    Calculate energy
    total the moons and velocity

--- src/aoc2019/day12/core.clj
+++ src/aoc2019/day12/core.clj
@@ -1,6 +1,26 @@
 (ns aoc2019.day12.core)
 
+(defn tick
+  "simulate motion of the moons"
+  [moons velocity]
+  [moons velocity])
+
+(defn abs
+  "get the absolute value"
+  [value]
+  (map #(Math/abs %1) value))
+
+(defn calculate
+  "calculate the energy"
+  [moons velocity]
+  (apply + (map *
+                (map #(apply + %1) (map abs moons))
+                (map #(apply + %1) (map abs velocity)))))
+
 (defn day12a
-  "calculate energy"
-  [moons steps]
-  0)
+  "find the energy after steps"
+  [moons velocity steps]
+  (if (= steps 0)
+    (calculate moons velocity)
+    (let [[a b] (tick moons velocity)]
+      (recur a b (dec steps)))))
--- test/aoc2019/day12_test.clj
+++ test/aoc2019/day12_test.clj
@@ -3,7 +3,11 @@
             [aoc2019.day12.core :refer :all]))
 
 (deftest day12
+  (testing "calculate"
+    (def expected 179)
+    (def actual (calculate [[2 1 -3] [1 -8 0] [3 -6 1] [2 0 4]] [[-3 -2 1] [-1 1 3] [3 2 -3] [1 -1 -1]]))
+    (is (= expected actual)))
   (testing "day12a"
     (def expected 179)
-    (def actual (day12a [[-1 0 2] [2 -10 -7] [4 -8 8] [3 5 -1]] 10))
+    (def actual (day12a [[-1 0 2] [2 -10 -7] [4 -8 8] [3 5 -1]] [[0 0 0] [0 0 0] [0 0 0] [0 0 0]] 10))
     (is (= expected actual))))
