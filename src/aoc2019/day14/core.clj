5188be01b6a2257ccf97ff059f101eecc9a105d9
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2020-02-21 22:29:12 +0100

    Parse input from file

--- src/aoc2019/day14/core.clj
+++ src/aoc2019/day14/core.clj
@@ -1,11 +1,25 @@
 (ns aoc2019.day14.core)
 
-(defn parse
-  "parse raw input into reactions"
+(defn string->reaction
+  "parse a string into a series of reactions"
   [raw]
-  [])
+  (let [[left right] (clojure.string/split raw #" => ")
+        commas (clojure.string/split left #", ")
+        keypairs (map (fn [x] (clojure.string/split x #"\s")) commas)
+        in-result (map (fn [[k v]] [(Integer/parseInt k) v]) keypairs)
+        [i o] (clojure.string/split right #"\s")]
+    {:in (vec in-result)
+     :out [(Integer/parseInt i) o]}))
+
+(defn parse
+  "parse file input into reactions"
+  [filename]
+  (let [raw (slurp filename)
+        lines (clojure.string/split raw #"\r\n")]
+    (map string->reaction lines)))
 
 (defn day14a
   "Find the amount of ore for 1 fuel"
   [reactions]
+  (println reactions)
   0)
--- test/aoc2019/day14_test.clj
+++ test/aoc2019/day14_test.clj
@@ -3,6 +3,14 @@
             [aoc2019.day14.core :refer :all]))
 
 (deftest day14
+  (testing "string->reaction"
+    (def expected {:in [[3 "A"] [4 "B"]] :out [1 "AB"]})
+    (def actual (string->reaction "3 A, 4 B => 1 AB"))
+    (is (= expected actual)))
+  (testing "string->reaction without comma"
+    (def expected {:in [[3 "A"]] :out [1 "AB"]})
+    (def actual (string->reaction "3 A => 1 AB"))
+    (is (= expected actual)))
   (testing "day14a"
     (def expected 0)
     (def actual (day14a (parse "src/aoc2019/day14/input.txt")))
