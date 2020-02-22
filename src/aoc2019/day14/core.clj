dfb287e312622fb15d32e8b5c2fe557e748932c9
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2020-02-22 10:03:57 +0100

    Add recursive lookup of ingredients for FUEL
    - Need to take into account the amount of material

--- src/aoc2019/day14/core.clj
+++ src/aoc2019/day14/core.clj
@@ -16,10 +16,35 @@
   [filename]
   (let [raw (slurp filename)
         lines (clojure.string/split raw #"\r\n")]
-    (map string->reaction lines)))
+    (vec (map string->reaction lines))))
+
+(defn has?
+  "filter based on a type"
+  [type {:keys [in out]}]
+  (let [[k v] out]
+    (= v type)))
+
+(defn produces?
+  "finds the fuel"
+  [type reactions]
+  (filter (partial has? type) reactions))
+
+(defn ore?
+  "does the ingredients contain ore?"
+  [[amount type]]
+  (= "ORE" type))
+
+(defn ore
+  "find the amount of ore for an ingredient"
+  [reactions [amount type]]
+  (let [ingredients (mapcat :in (produces? type reactions))
+        os (first (filter ore? ingredients))]
+    (if (nil? os)
+      (reduce (fn [acc n] (+ acc (ore reactions n))) 0 ingredients)
+      (first os))))
 
 (defn day14a
   "Find the amount of ore for 1 fuel"
   [reactions]
-  (println reactions)
-  0)
+  (let [ingredients (:in (first (produces? "FUEL" reactions)))]
+    ingredients))
--- test/aoc2019/day14_test.clj
+++ test/aoc2019/day14_test.clj
@@ -3,7 +3,7 @@
             [aoc2019.day14.core :refer :all]))
 
 (deftest day14
-  (testing "string->reaction"
+  (testing "string->reaction with comma"
     (def expected {:in [[3 "A"] [4 "B"]] :out [1 "AB"]})
     (def actual (string->reaction "3 A, 4 B => 1 AB"))
     (is (= expected actual)))
@@ -11,7 +11,23 @@
     (def expected {:in [[3 "A"]] :out [1 "AB"]})
     (def actual (string->reaction "3 A => 1 AB"))
     (is (= expected actual)))
+  (testing "ore?"
+    (def expected true)
+    (def actual (ore? [7 "ORE"]))
+    (is (= expected actual)))
+  (testing "ore?"
+    (def expected false)
+    (def actual (ore? [7 "WE"]))
+    (is (= expected actual)))
+  ;; (testing "ore"
+    ;; (def expected 10)
+    ;; (def actual (ore (parse "src/aoc2019/day14/input.txt") [7 "A"]))
+    ;; (is (= expected actual)))
+  (testing "ore"
+    (def expected 31)
+    (def actual (ore (parse "src/aoc2019/day14/input.txt") [1 "FUEL"]))
+    (is (= expected actual)))
   (testing "day14a"
-    (def expected 0)
+    (def expected [])
     (def actual (day14a (parse "src/aoc2019/day14/input.txt")))
     (is (= expected actual))))
