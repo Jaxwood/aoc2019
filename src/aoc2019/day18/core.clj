88b34967f255d0312bd46deb2ae3b02bf143c60c
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2020-03-02 20:10:36 +0100

    Parse the vault

--- src/aoc2019/day18/core.clj
+++ src/aoc2019/day18/core.clj
@@ -1,11 +1,28 @@
-(ns aoc2019.day18.core)
+(ns aoc2019.day18.core
+  (:require [clojure.string :refer [split-lines]]))
+
+(defn tile
+  "find the tile"
+  [obstacle]
+  (cond
+    (= obstacle "#") :wall
+    (= obstacle ".") :open
+    (= obstacle "@") :entrance
+    :else (if (nil? (re-seq #"[A-Z]+" obstacle))
+            :key
+            :door)))
 
 (defn parse
-  "parse the maze"
-  [input]
-  [])
+  "parse the vault"
+  [filename]
+  (let [lines (split-lines (slurp filename))]
+    (loop [line lines acc {} y 0]
+      (if (empty? line)
+        acc
+        (let [coords (map-indexed (fn [idx itm] [[idx y] (tile (str itm))]) (first line))]
+          (recur (rest line) (into acc coords) (dec y)))))))
 
 (defn day18a
   "solution for day18a"
-  [maze]
-  0)
+  [vault]
+  vault)
