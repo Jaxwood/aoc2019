ee2938da2be9853761d90bc95c3c878106bec995
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2020-03-07 07:38:54 +0100

    Improve algorithm
    Find all the keys and the distance between them.
    Iterate all possible combinations (which is not a good idea)

--- src/aoc2019/day18/core.clj
+++ src/aoc2019/day18/core.clj
@@ -1,5 +1,5 @@
 (ns aoc2019.day18.core
-  (:require [clojure.string :refer [split-lines upper-case]]))
+  (:require [clojure.string :refer [split-lines lower-case]]))
 
 (defn tile
   "find the tile"
@@ -38,7 +38,7 @@
   [k]
   (-> k
       name
-      upper-case
+      lower-case
       keyword))
 
 (defn neighbors
@@ -50,58 +50,85 @@
         acc
         (let [next (first lst)
               obstacle (get vault next)]
-          (if (or (wall? obstacle) (door? obstacle))
+          (if (wall? obstacle)
             (recur (rest lst) acc)
             (recur (rest lst) (conj acc next))))))))
 
-(defn unvisited?
-  "filter nodes that has yet to be visited"
-  [{:keys [vault current from visited]}]
-  (let [adjecent (neighbors vault current)]
-    (filter #(nil? (get visited %)) adjecent)))
+(defn visited?
+  "has the tile been visited?"
+  [visited location]
+  (nil? (get visited location)))
 
-(defn open
-  "opens a door"
-  [{:keys [vault current from visited]}]
-  (let [obstacle (get vault current)
-        [door _] (first (filter (fn [[_ type]] (= type (unlocks obstacle))) vault))]
-    (if (nil? door)
-      (assoc vault current :open)
-      (assoc vault current :open door :open))))
+(defn add-door
+  "add doors"
+  [obstacle acc]
+  (if (door? obstacle)
+    (conj acc obstacle)
+    acc))
 
-(defn explored?
-  "is the vault fully explored?"
-  [vault]
-  (let [remaining (filter (fn [[_ type]] (key? type)) vault)]
-    (= (count remaining) 0)))
-
-(defn moves
-  "transform moves into the next state"
-  [{:keys [vault from current visited]} move adjecent]
-  (map (fn [pos] {:vault vault :from current :current pos :visited (conj visited [current (inc move)])}) adjecent))
-
-(defn traverse
-  "traverse the vault"
-  [tovisit acc]
-  (if (empty? tovisit)
-    (apply min acc)
-    (let [{:keys [vault current from visited], :as candidate} (first tovisit)
-          move (get visited from)
-          obstacle (get vault current)]
+(defn explore
+  "explore the vault"
+  [vault moves visited acc]
+  ;;(Thread/sleep 2000)
+  (if (empty? moves)
+    acc
+    (let [[current from doors] (first moves)
+          distance (get visited from)
+          obstacle (get vault current)
+          moredoors (add-door obstacle doors)
+          next (map (fn [pos] [pos current moredoors]) (filter (partial visited? visited) (neighbors vault current)))]
       (if (key? obstacle)
-        (let [updated-candidate (assoc candidate :visited {} :vault (open candidate))
-              adjecent (unvisited? updated-candidate)
-              next-moves (moves updated-candidate move adjecent)]
-          (if (explored? (:vault updated-candidate))
-            (recur (rest tovisit) (conj acc (inc move)))
-            (recur (into (rest tovisit) next-moves) acc)))
-        (let [adjecent (unvisited? candidate)
-              next-moves (moves candidate move adjecent)]
-          (recur (into (rest tovisit) next-moves) acc))))))
+        (recur vault (into (rest moves) next) (conj visited [current (inc distance)]) (conj acc [obstacle (inc distance) moredoors]))
+        (recur vault (into (rest moves) next) (conj visited [current (inc distance)]) acc)))))
+
+(defn routes
+  "map all the routes and the distances of the vault"
+  [vault keyring]
+  (loop [ks keyring acc {}]
+    (if (empty? ks)
+      acc
+      (let [candidate (first ks)
+            [current _] (first (filter (fn [[_ v]] (= v candidate)) vault))
+            moves (map (fn [pos] [pos current []]) (neighbors vault current))]
+        (recur (rest ks) (into acc {candidate (explore vault moves {current 0} [])}))))))
+
+(defn access?
+  ""
+  [visited [k _ ds]]
+  (if (contains? (into #{} visited) k)
+    false
+    (every? (fn [d] (contains? (into #{} visited) (unlocks d))) ds)))
+
+(defn score
+  "calculate score for a breadcrumb"
+  [paths breadcrumbs acc]
+  (if (= 1 (count breadcrumbs))
+    acc
+    (let [a (first breadcrumbs)
+          b (second breadcrumbs)
+          [_ v _] (first (filter (fn [[k v _]] (= k b)) (a paths)))]
+      (recur paths (rest breadcrumbs) (+ acc v)))))
+
+
+(defn travel
+  ""
+  [paths acc scores]
+  (if (empty? acc)
+    (apply min scores)
+    (let [breadcrumbs (first acc) ;; [[:current]]
+          next (last breadcrumbs) ;; :current
+          connected (filter (partial access? breadcrumbs) (next paths))
+          sorted (sort-by second connected)
+          ways (map (fn [n] (conj breadcrumbs (first n))) sorted)]
+      (if (empty? connected)
+        (recur paths (rest acc) (conj scores (score paths breadcrumbs 0)))
+        (recur paths (into (rest acc) ways) scores)))))
 
 (defn day18a
   "solution for day18a"
   [vault]
-  (let [[current _] (first (filter (comp current? second) vault))
-        tovisit (map (fn [pos] {:vault vault :current pos :from current :visited {current 0}}) (neighbors vault current))]
-    (traverse (into #{} tovisit) #{})))
+  (let [keyring (conj (map second (filter (fn [[_ v]] (key? v)) vault)) :current)
+        paths (routes vault keyring)]
+    (travel paths [[:current]] [])))
+
+    ;; (next paths) => [[:a 2 []] [:b 4 [:A]]]
--- test/aoc2019/day18_test.clj
+++ test/aoc2019/day18_test.clj
@@ -3,22 +3,6 @@
             [aoc2019.day18.core :refer :all]))
 
 (deftest day18
-  (testing "unlocks"
-    (let [expected :A
-          actual (unlocks :a)]
-      (is (= expected actual))))
-  (testing "open with key and door"
-    (let [expected {[0 0] :open [1 1] :open}
-          actual (open {:vault {[0 0] :a [1 1] :A} :current [0 0] :from [1 1] :visited {}})]
-      (is (= expected actual))))
-  (testing "open with key only"
-    (let [expected {[0 0] :open [1 1] :wall}
-          actual (open {:vault {[0 0] :a [1 1] :wall} :current [0 0] :from [1 1] :visited {}})]
-      (is (= expected actual))))
-  (testing "unvisited?"
-    (let [expected [[1 0]]
-          actual (unvisited? {:vault {[0 0] :open [0 1] :wall [0 -1] :wall [1 0] :open [-1 0] :wall} :current [0 0] :from [1 1] :visited {}})]
-      (is (= expected actual))))
    (testing "day18a testcase a"
      (let [expected 8
            actual (day18a (parse "src/aoc2019/day18/a.txt"))]
