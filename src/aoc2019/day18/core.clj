304732299bb5b84862d8a6b66ccf2b92b6d19783
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2020-03-09 19:37:44 +0100

    Add explore function

--- src/aoc2019/day18/core.clj
+++ src/aoc2019/day18/core.clj
@@ -1,5 +1,6 @@
 (ns aoc2019.day18.core
-  (:require [clojure.string :refer [split-lines]]))
+  (:require [clojure.string :refer [lower-case split-lines]]
+            [clojure.set :refer [difference]]))
 
 (defn tile
   "parse the tile"
@@ -21,15 +22,46 @@
               result (map-indexed (fn [x character] [[x y] (tile (str character))]) line)]
           (recur (rest lines) (dec y) (into acc result)))))))
 
+(def all-keys (into #{} (map (comp keyword str char) (range 97 123))))
+(def all-doors (into #{} (map (comp keyword str char) (range 65 91))))
 (defn left [[x y]] [(dec x) y])
 (defn right [[x y]] [(inc x) y])
 (defn up [[x y]] [x (inc y)])
 (defn down [[x y]] [x (dec y)])
+(defn key? [candidate] (contains? all-keys candidate))
+(defn door? [candidate] (contains? all-doors candidate))
 (defn wall? [vault pos] (= :wall (get vault pos)))
 (def not-wall? (complement wall?))
 
 (defn neighbors
-  "find passable neighbors tiles"
+  "find passable neighbor tiles"
   [vault pos]
-  (let [movements ((juxt left right up down) pos)]
-    (filter (partial not-wall? vault) movements)))
+  (let [movements (juxt left right up down)]
+    (filter (partial not-wall? vault) (movements pos))))
+
+(defn with-moves
+  "add the movement count to the coordiate"
+  [m doors coords]
+  (map (fn [xs] (conj (conj xs m) doors)) coords))
+
+(defn with-doors
+  "add the doors"
+  [type acc]
+  (if (door? type)
+    (conj acc (keyword (lower-case (name type))))
+    acc))
+
+(defn explore
+  "find the keys"
+  [vault from]
+  (let [tiles (with-moves 1 [] (neighbors vault from))]
+    (loop [moves tiles visited #{from} acc {}]
+      (if (empty? moves)
+        acc
+        (let [[x y m doors] (first moves)
+              type (get vault [x y])
+              next (difference (set (neighbors vault [x y])) visited)
+              next-moves (with-moves (inc m) (with-doors type doors) next)]
+          (if (key? type)
+            (recur (into (rest moves) next-moves) (conj visited [x y]) (into acc {type {:cost m :dependsOn doors}}))
+            (recur (into (rest moves) next-moves) (conj visited [x y]) acc)))))))
--- test/aoc2019/day18_test.clj
+++ test/aoc2019/day18_test.clj
@@ -3,29 +3,33 @@
             [aoc2019.day18.core :refer :all]))
 
 (deftest day18
-   (testing "parse"
-     (let [expected  {[0 0] :wall [1 0] :wall [2 0] :wall [3 0] :wall [4 0] :wall [5 0] :wall [6 0] :wall [7 0] :wall [8 0] :wall
-                      [0 -1] :wall [1 -1] :b [2 -1] :open [3 -1] :A [4 -1] :open [5 -1] :current [6 -1] :open [7 -1] :a [8 -1] :wall
-                      [0 -2] :wall [1 -2] :wall [2 -2] :wall [3 -2] :wall [4 -2] :wall [5 -2] :wall [6 -2] :wall [7 -2] :wall [8 -2] :wall}
-           actual (parse "src/aoc2019/day18/a.txt")]
-       (is (= expected actual))))
-   (testing "left"
-     (let [expected [-1 0]
-           actual (left [0 0])]
-       (is (= expected actual))))
-   (testing "right"
-     (let [expected [1 0]
-           actual (right [0 0])]
-       (is (= expected actual))))
-   (testing "up"
-     (let [expected [0 1]
-           actual (up [0 0])]
-       (is (= expected actual))))
-   (testing "down"
-     (let [expected [0 -1]
-           actual (down [0 0])]
-       (is (= expected actual))))
-   (testing "neighbors"
-     (let [expected [[4 -1] [6 -1]]
-           actual (neighbors (parse "src/aoc2019/day18/a.txt") [5 -1])]
-       (is (= expected actual)))))
+  (testing "parse"
+    (let [expected  {[0 0] :wall [1 0] :wall [2 0] :wall [3 0] :wall [4 0] :wall [5 0] :wall [6 0] :wall [7 0] :wall [8 0] :wall
+                     [0 -1] :wall [1 -1] :b [2 -1] :open [3 -1] :A [4 -1] :open [5 -1] :current [6 -1] :open [7 -1] :a [8 -1] :wall
+                     [0 -2] :wall [1 -2] :wall [2 -2] :wall [3 -2] :wall [4 -2] :wall [5 -2] :wall [6 -2] :wall [7 -2] :wall [8 -2] :wall}
+          actual (parse "src/aoc2019/day18/a.txt")]
+      (is (= expected actual))))
+  (testing "left"
+    (let [expected [-1 0]
+          actual (left [0 0])]
+      (is (= expected actual))))
+  (testing "right"
+    (let [expected [1 0]
+          actual (right [0 0])]
+      (is (= expected actual))))
+  (testing "up"
+    (let [expected [0 1]
+          actual (up [0 0])]
+      (is (= expected actual))))
+  (testing "down"
+    (let [expected [0 -1]
+          actual (down [0 0])]
+      (is (= expected actual))))
+  (testing "neighbors"
+    (let [expected [[4 -1] [6 -1]]
+          actual (neighbors (parse "src/aoc2019/day18/a.txt") [5 -1])]
+      (is (= expected actual))))
+  (testing "explore"
+    (let [expected {:a {:cost 2 :dependsOn []} :b {:cost 4 :dependsOn [:a]}}
+          actual (explore (parse "src/aoc2019/day18/a.txt") [5 -1])]
+      (is (= expected actual)))))
