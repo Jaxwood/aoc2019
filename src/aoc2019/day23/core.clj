c90bff46c51a1985d3c1794ad21743b5f1c52913
Jacob Lorenzen <jacob@lorenzen.me>
2020-04-15 20:43:42 +0200

    Solution for day23b

--- src/aoc2019/day23/core.clj
+++ src/aoc2019/day23/core.clj
@@ -28,6 +28,28 @@
           (= (:status program) :uninterruptible)
           (let [x (run program)
                 y (run x)]
+            (if (= (:output program) 255)
+              (:output y)
+              (recur idx (assoc network (first msg) y) (conj (vec (rest queue)) [(:output program) (:output x) (:output y)])))))))))
+
+(defn day23b
+  "find the Y-value send twice to the NAT"
+  [memory]
+  (let [nics (into {} (map (fn [idx] [idx (run {:memory memory :relative 0 :address 0 :input [idx]})]) (range 50)))]
+    (loop [idx 0 network nics queue [] nat #{} idle 0]
+      (if (> idle 50)
+        (let [[a x y, :as zero] (get network 255)]
+          (if (contains? nat y)
+            y
+            (recur idx network (conj queue zero) (conj nat y) 0)))
+        (let [msg (transmit queue idx)
+              program (run (assoc (get network (first msg)) :input (rest msg)))]
+          (cond
+            (= (:status program) :interruptible)
+            (recur (inc-index idx) (assoc network (first msg) program) (rest queue) nat (inc idle))
+            (= (:status program) :uninterruptible)
+            (let [x (run program)
+                  y (run x)]
               (if (= (:output program) 255)
-                (:output y)
-                (recur idx (assoc network (first msg) y) (conj queue [(:output program) (:output x) (:output y)])))))))))
+                (recur idx (assoc (assoc network (first msg) y) 255 [0 (:output x) (:output y)]) (rest queue) nat 0)
+                (recur idx (assoc network (first msg) y) (conj (vec (rest queue)) [(:output program) (:output x) (:output y)]) nat 0)))))))))
--- test/aoc2019/day23_test.clj
+++ test/aoc2019/day23_test.clj
@@ -7,4 +7,8 @@
   (testing "day23a"
     (let [expected 18513
           actual (day23a (parse "src/aoc2019/day23/input.txt"))]
+      (is (= expected actual))))
+  (testing "day23b"
+    (let [expected 13286
+          actual (day23b (parse "src/aoc2019/day23/input.txt"))]
       (is (= expected actual)))))
